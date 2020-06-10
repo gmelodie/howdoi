88e194b5676262224db0337c61013bff05daf5ad
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-06-11 01:14:48 +0200

    Add server-client api placeholder

---
+++ howdoi2/server/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Query(BaseModel):
+    content: str
+    other_content: str = None # optional
+
+
+client_app = FastAPI(
+    title="Howdoi Clients",
+    description="Howdoi's API for clients",
+    version="1",
+)
+
+
+@client_app.post('/plugins')
+async def root(query: Query):
+    return {
+        "Hello": "World",
+        "content": query.content,
+    }
+
+
+@client_app.post('/plugins/{plugin}')
+async def plugin(plugin: str, query: Query):
+    return {
+        "plugin": plugin,
+        "content": query.content,
+    }
