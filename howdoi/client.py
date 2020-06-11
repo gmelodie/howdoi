27531a9e71ea70db2d0ae8b960b772f2a3812a5c
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-06-11 19:13:28 +0200

    Add client's skeleton

--- howdoi/client.py
+++ howdoi/client.py
@@ -1,6 +1,14 @@
+import os
 import requests
 
 
+SERVER_URL = os.getenv("HOWDOI_SERVER_URL",
+                       default="http://localhost:8000/plugins/")
+
 
 if __name__ == '__main__':
-    pass
+    query = input("Type query: ")
+    response = requests.post(SERVER_URL, json={"query_str": query})
+    print(response.json()["answer_str"])
+    # for key, value in response.json().items():
+    #     print(key, value)
