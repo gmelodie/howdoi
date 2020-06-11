e67c147c0b68be689ade8f2ac1d55296f0f710e5
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-06-11 20:47:52 +0200

    Add suggested testing structure

--- howdoi/test_server.py
+++ howdoi/test_server.py
@@ -6,64 +6,52 @@ from server import app
 client = TestClient(app)
 
 
+TEST_QUERIES = [
+    {
+        "query_str":"test",
+        "other_content":"some other content",
+        "expected": {
+            "code": 200,
+        }
+    },
+    {
+        "query_str": None,
+        "other_content":"some other content",
+        "expected": {
+            "code": 422,
+        }
+    },
+]
+
+
 def test_plugins():
     """Test general /plugins endpoint"""
-    good_response = client.post(
-        "/plugins",
-        headers={},
-        json={
-            "query_str": "test",
-            "other_content": "some other content"
-        },
-    )
 
-    bad_response = client.post(
-        "/plugins",
-        headers={},
-        json={
-            # no content (required)
-            "other_content": "some other content"
-        },
-    )
-
-    assert good_response.json() == {
-        "answer_str": "@Parameters(name=\"namestring\")\n",
-        "plugin": None,
-        "other_content": None,
-    }
-    assert good_response.status_code == 200
-
-    assert bad_response.status_code == 422 # unprocessable
+    for test in TEST_QUERIES:
+        response = client.post(
+            "/plugins",
+            headers={},
+            json={
+                "query_str": test["query_str"],
+                "other_content": test["other_content"]
+            },
+        )
+        assert response.status_code == test["expected"]["code"]
 
 
 def test_plugin():
     """Test specific /plugins/{plugin} endpoint"""
-    good_response = client.post(
-        "/plugins/testplugin",
-        headers={},
-        json={
-            "query_str": "test",
-            "other_content": "some other content"
-        },
-    )
 
-    bad_response = client.post(
-        "/plugins/testplugin",
-        headers={},
-        json={
-            # no content (required)
-            "other_content": "some other content"
-        },
-    )
-
-    assert good_response.json() == {
-        "answer_str": "@Parameters(name=\"namestring\")\n",
-        "plugin": "testplugin",
-        "other_content": None,
-    }
-    assert good_response.status_code == 200
-
-    assert bad_response.status_code == 422 # unprocessable
+    for test in TEST_QUERIES:
+        response = client.post(
+            "/plugins/testplugin",
+            headers={},
+            json={
+                "query_str": test["query_str"],
+                "other_content": test["other_content"]
+            },
+        )
+        assert response.status_code == test["expected"]["code"]
 
 
 def test_root():
